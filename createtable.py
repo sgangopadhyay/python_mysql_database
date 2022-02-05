ce154856722d11678efbafb164661b875b387631
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-02-05 18:04:49 +0530

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,7 +1,12 @@
 # Program to create a TABLE
 import pymysql
 try:
-    db = pymysql.connect("localhost", "root", "suman", "food");
+    db = db = pymysql.connect(
+            host="localhost",
+            user="root",
+            password="suman",
+            db="guvi"
+        )
     c = db.cursor();
     print("Database Connection Successfull");
     sql = """CREATE TABLE FOOD_NAME(ID INT(3), ITEM VARCHAR(20), COUNTRY VARCHAR(20))"""
